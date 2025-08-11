e0c61c903a20d9303793af35dce4881552c50f5d
maruki00 <aitbaha48@gmail.com>
2025-08-11 19:12:20 +0100

    commit

--- rpisec/csci4968/lvl1/lab1/src.c
+++ rpisec/csci4968/lvl1/lab1/src.c
@@ -1,60 +1,38 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 /*
  * compiled with:
- * gcc -O0 -fno-stack-protector lab2A.c -o lab2A
+ * gcc -O0 -fno-stack-protector src.c -o foo
  */
 
 void shell()
 {
-	printf("You got it\n");
+	printf("You did it.\n");
 	system("/bin/sh");
-}
-
-void concatenate_first_chars()
-{
-	struct {
-		char word_buf[12];
-		int i;
-		char* cat_pointer;
-		char cat_buf[10];
-	} locals;
-	locals.cat_pointer = locals.cat_buf;
-
-	printf("Input 10 words:\n");
-	for(locals.i=0; locals.i!=10; locals.i++)
-	{
-		// Read from stdin
-		if(fgets(locals.word_buf, 0x10, stdin) == 0 || locals.word_buf[0] == '\n')
-		{
-			printf("Failed to read word\n");
-			return;
-		}
-		// Copy first char from word to next location in concatenated buffer
-		*locals.cat_pointer = *locals.word_buf;
-		locals.cat_pointer++;
-	}
-
-	// Even if something goes wrong, there's a null byte here
-	//   preventing buffer overflows
-	locals.cat_buf[10] = '\0';
-	printf("Here are the first characters from the 10 words concatenated:\n\
-%s\n", locals.cat_buf);
 }
 
 int main(int argc, char** argv)
 {
-	if(argc != 1)
+	if(argc != 2)
 	{
-		printf("usage:\n%s\n", argv[0]);
+		printf("usage:\n%s string\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	concatenate_first_chars();
+	int set_me = 0;
+	char buf[15];
+	strcpy(buf, argv[1]);
 
-	printf("Not authenticated\n");
+	if(set_me == 0xdeadbeef)
+	{
+		shell();
+	}
+	else
+	{
+		printf("Not authenticated.\nset_me was %d\n", set_me);
+	}
+
 	return EXIT_SUCCESS;
 }
-
